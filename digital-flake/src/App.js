dd001ca39cc5779699b3f2d89e481ce83c9c4391
Ajinath <ajinath2151@gmail.com>
2024-02-10 01:27:08 +0530

    start working on login form

--- digital-flake/src/App.js
+++ digital-flake/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+    <LoginPage />    
+    </>
   );
 }
 
---
+++ digital-flake/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography } from '@mui/material';
+
+const LoginPage = () => {
+  return (
+    <div>      
+      <Typography variant='h2' sx={{color:'red'}}>thi this is login page</Typography>
+    </div>
+  )
+}
+
+export default LoginPage
