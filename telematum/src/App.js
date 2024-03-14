ce2288ad8972d4849a43d776503243398c49fb4d
Ajinath <ajinath2151@gmail.com>
2024-03-15 01:32:22 +0530

    showing static data in table

--- telematum/src/App.js
+++ telematum/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import DataTable from './components/DataTable';
 
 function App() {
   return (
     <div>
-      working started...
+      <DataTable />
     </div>
   );
 }
