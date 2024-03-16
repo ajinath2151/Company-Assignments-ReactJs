d3525b3b8fd6b12e01337467fe3b1dcd6529ad74
Ajinath <ajinath2151@gmail.com>
2024-03-16 22:06:39 +0530

    adding some comments and fill hard data to last column (may be incorrect)

--- telematum/src/components/EmployeeDataTable.js
+++ telematum/src/components/EmployeeDataTable.js
@@ -9,16 +9,23 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { GridDeleteIcon } from "@mui/x-data-grid";
+
 const EmployeeDataTable = () => {
+
+  // columns array with object type values i.e. array of objects for column names
   const columns = [
-    { id: "patient_name", name: "patient_name" },
-    { id: "appointment_date", name: "appointment_date" },
-    { id: "appointment_time", name: "appointment_time" },
-    { id: "doctor", name: "doctor" },
-    { id: "injury", name: "injury" },
-    { id: "action", name: "action" },
+    { id: "patient_name", name: "PATIENT" },
+    { id: "appointment_date", name: "DATE" },
+    { id: "appointment_time", name: "TIME" },
+    { id: "doctor", name: "DOCTOR" },
+    { id: "injury", name: "INJURY" },
+    { id: "action", name: "ACTION" },
   ];
 
+  // this is useState for rows data
   const [rows, rowsChange] = useState([]);
 
   useEffect(() => {
@@ -31,33 +38,47 @@ const EmployeeDataTable = () => {
         console.log(response.data.appointments);
       });
   }, []);
+
+  const showAlarm = (event) => {
+    event.preventDefault();
+    alert("jjjjjjjjjjjj");
+  }
   return (
+    
     <div style={{ textAlign: "center" }}>
       <Typography variant={"h2"}>Employee Data Table</Typography>
-
+      {/* paper for styling purposes */}
       <Paper sx={{ width: "90%", marginLeft: 5 }}>
         <TableContainer>
           <Table>
             <TableHead>
               <TableRow>
                 {columns?.map((colum) => (
+                  // table cell means column
                   <TableCell key={colum.id}>{colum.name}</TableCell>
+                  
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
               {rows && rows.map((row,index)=>{
                 return (
-                  <TableRow key={index}>
+                  <>
+                  <TableRow key={index} >
                     {columns && columns.map((colum,index)=>{
-                      let value = row[colum.id];
+                      let value = row[colum.id];                      
                       return (
                         <TableCell key={value}>
-                          {value}
+                          {value}   
                         </TableCell>
                       )
                     }) }
+                    
+                    
+                    <MoreVertIcon style={{}} onClick={showAlarm}/>
                   </TableRow>
+                  
+                    </>
                 )
               })}
             </TableBody>
