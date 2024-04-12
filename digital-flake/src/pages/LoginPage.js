f7545f8c708bcf82e7855a41ff15ca6310e9aa07
Ajinath <ajinath2151@gmail.com>
2024-04-12 19:02:59 +0530

    validate email id and password should be filled before click on login otherwise shown alert

--- digital-flake/src/pages/LoginPage.js
+++ digital-flake/src/pages/LoginPage.js
@@ -20,7 +20,7 @@ import digitalFlake from "../images/logo/digitalFlake.png";
 import TextField from "@mui/material/TextField";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import styled from "styled-components";
-import './LoginPage.css';
+import "./LoginPage.css";
 
 // Styled Container for div style
 const TopDiv = styled.div({
@@ -34,24 +34,22 @@ const TopDiv = styled.div({
 
 //component name LoginPage
 const LoginPage = () => {
-
   // show/hide password on click of eya icon
   const [showPassword, setShowPassword] = React.useState(false);
   // for email inside forget Password combo box
   const [email, setEmail] = useState(""); // State variable to hold email address
   // useState for open dialog box on click of forget / forgot password
   const [open, setOpen] = useState(false);
-  
 
-    // for open dialog box onClick of forgot Password
-  const handleClickOpen = (e) =>{
+  // for open dialog box onClick of forgot Password
+  const handleClickOpen = (e) => {
     e.preventDefault();
-    setOpen(true)        
-  }
+    setOpen(true);
+  };
   // for close dialog box onClick of cancel inside forgot Password link dialog box
-  const handleClose = () =>{
+  const handleClose = () => {
     setOpen(false);
-  }
+  };
 
   // for email inside forget Password combo box
   const handleSubmit = (event) => {
@@ -62,28 +60,37 @@ const LoginPage = () => {
     setEmail(emailValue); // Store the email address in state
     console.log(emailValue);
     handleClose();
-    alert("password link sent to your email")
+    alert("password link sent to your email");
     // if (emailValue!="") {
     //   alert("password link sent to your email")
     // }
   };
 
-  // for show / hide password on click of eye icon of password 
+  // for show / hide password on click of eye icon of password
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-  // for prevent default password on click of eye icon of password 
+
+  // for prevent default password on click of eye icon of password
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
   const handleLogin = (event) => {
     event.preventDefault();
-    alert("Login");        
-  }
+    const emailValue = document.getElementById("emailIdLoginPage").value;
+    const passwordValue = document.getElementById("passwordLoginPage").value;
+  
+    if (!emailValue || !passwordValue) {
+      alert("Please fill in both email and password fields.");
+    } else {
+      alert("Login");
+      // Proceed with login logic
+    }
+  };
+  
 
   return (
     // all data of LoginPage will be return / available under this container
-    <Container id="myid" style={{marginTop:50}}>
+    <Container id="myid" style={{ marginTop: 50 }}>
       <Fragment>
         {/* define paper size for show data */}
         <Paper
@@ -95,7 +102,7 @@ const LoginPage = () => {
             justifyContent: "center",
           }}
         >
-           {/* inside FormControl having all controls of form like TextField label and all */}
+          {/* inside FormControl having all controls of form like TextField label and all */}
           <FormControl>
             {/* <FormControl style={{border:'1px solid black', height:'250px'}}> */}
             {/* the content above textfield is in top div */}
@@ -111,19 +118,24 @@ const LoginPage = () => {
             {/* ./ the content above textfield is in top div */}
 
             {/* email id textfield LoginPage */}
-            <TextField                          
-              id="outlined-required"
+            <TextField
+              id="emailIdLoginPage"
+              required
               label="Email ID"
-              type="email"              
+              name="email"
+              type="email"
               autoComplete="current-password"
-              style={{marginTop:20, marginBottom:20}}
-            />     
+              style={{ marginTop: 20, marginBottom: 20 }}
+            />             
             {/* password textfield LoginPage         */}
             <FormControl variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <InputLabel htmlFor="outlined-adornment-password" required>
+                Password
+              </InputLabel>
               <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? 'text' : 'password'}
+                id="passwordLoginPage"
+                required
+                type={showPassword ? "text" : "password"}
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
@@ -140,49 +152,82 @@ const LoginPage = () => {
               />
               {/* forgot ,forget password link */}
               {/* <Link style={{position:'absolute', right:0, top:74, textDecoration:'none'}} onClick={()=>{alert("this is password change root")}}>Forgot password?</Link> */}
-              <Link style={{position:'absolute', right:0, top:74, textDecoration:'none'}} onClick={handleClickOpen}>Forgot password?</Link>
+              <Link
+                style={{
+                  position: "absolute",
+                  right: 0,
+                  top: 74,
+                  textDecoration: "none",
+                }}
+                onClick={handleClickOpen}
+              >
+                Forgot password?
+              </Link>
               {/* dialog open on click of forget password */}
-              <Dialog              
+              <Dialog
                 open={open}
                 onClose={handleClose}
                 PaperProps={{
-                  component: 'form',
-                  onSubmit : handleSubmit,            
-                }}          
+                  component: "form",
+                  onSubmit: handleSubmit,
+                }}
               >
-                
-                  {/* forget password dialog box dialog title      */}
-                <DialogTitle sx={{color:'purple',textAlign:'center'}}>Did you forget your password?</DialogTitle>
-                  {/* forget password dialog box dialog content      */}
+                {/* forget password dialog box dialog title      */}
+                <DialogTitle sx={{ color: "purple", textAlign: "center" }}>
+                  Did you forget your password?
+                </DialogTitle>
+                {/* forget password dialog box dialog content      */}
                 <DialogContent>
                   {/* forget password dialog box dialog text      */}
-                  <DialogContentText sx={{paddingBottom:0}}>
-                    Enter your email address and we'll send you a link to reset password
-                  </DialogContentText>     
+                  <DialogContentText sx={{ paddingBottom: 0 }}>
+                    Enter your email address and we'll send you a link to reset
+                    password
+                  </DialogContentText>
                   {/* forget password dialog box email id      */}
                   <TextField
                     label="Email address"
                     fullWidth
                     id="email"
-                    name="email"                    
+                    name="email"
                     type="email"
                     required
                   />
                 </DialogContent>
-                  <Button type="submit" variant="" fullWidth sx={{height:50, background:'#753081', color:'white', marginTop:2}}>Request reset link</Button>
-                  <Button onClick={handleClose} sx={{display:'flex',justifyContent:'center'}}>Back to login</Button>
-                <DialogActions>
-                </DialogActions>
+                <Button
+                  type="submit"
+                  variant=""
+                  fullWidth
+                  sx={{
+                    height: 50,
+                    background: "#753081",
+                    color: "white",
+                    marginTop: 2,
+                  }}
+                >
+                  Request reset link
+                </Button>
+                <Button
+                  onClick={handleClose}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  Back to login
+                </Button>
+                <DialogActions></DialogActions>
               </Dialog>
             </FormControl>
             {/* login button of Login Page and click handleLogin event on click*/}
-            <Button  style={{marginTop:60, background:'purple'}} variant="contained" onClick={handleLogin}>Log In</Button>
-            
-          </FormControl>          
+            <Button
+              style={{ marginTop: 60, background: "purple" }}
+              variant="contained"
+              onClick={handleLogin}              
+            >
+              Log In
+            </Button>
+          </FormControl>
         </Paper>
       </Fragment>
     </Container>
   );
-}; 
+};
 
 export default LoginPage;
